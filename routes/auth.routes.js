65764f0b27252de17800057160ccc37bf873fc4c
SakhileMnisi88 <ragerwzrd77@gmail.com>
2026-04-08 02:21:51 +0200

    latest push

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,16 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const authRouter= Router();
 
-authRouter.post('sign-up', (req, res) => res.send({title: 'Sign Up'}));
-authRouter.post('sign-up', (req, res) => res.send({title: 'Sign in'}));
-authRouter.post('sign-up', (req, res) => res.send({title: 'Sign out'}));
+import {signIn, signOut, signUp} from '../controllers/auth.controller.js';
+
+//Path: /api/v1/auth/sign-up (POST)
+authRouter.post('/sign-up', signUp);
+
+//Path: /api/v1/auth/sign-in (POST)
+authRouter.post('/sign-in', signIn, );
+
+//Path: /api/v1/auth/sign-out (POST)
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
